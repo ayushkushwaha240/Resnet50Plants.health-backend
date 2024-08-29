b3713f64036bbbc405ed66c43b4e82624e6f0c92
Ayush Kushwaha <148432773+ayushkushwaha240@users.noreply.github.com>
2024-08-30 00:39:28 +0530

    model added

--- plants.health-backend/helper.py
+++ plants.health-backend/helper.py
@@ -3,40 +3,10 @@ from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.resnet50 import preprocess_input
 import numpy as np
 import requests
-from dotenv import load_dotenv
-import os
-from bs4 import BeautifulSoup
-
-url = os.getenv('url')
-
-def download_file_from_google_drive(destination):
-    response = requests.get(url, stream=True)
-    if response.status_code == 200:
-        with open(destination, 'wb') as file:
-            for chunk in response.iter_content(chunk_size=8192):
-                file.write(chunk)
-    else:
-        print(f"Failed to download file. Status code: {response.status_code}")
-        return None
 
 def load_model():
-    # Get the directory of the current script
-    current_directory = os.path.dirname(__file__)
-    # Construct the full path for the destination file
-    destination = os.path.join(current_directory, 'plantdiseasepredictionResnet50.h5')
-    
-    # Download the file
-    download_file_from_google_drive(destination)
-    
-    # Load the model
-    if os.path.exists(destination):
-        model = tf.keras.models.load_model(destination)
-        return model
-    else:
-        print("Model file was not found.")
-        return None
-
-
+    model = tf.keras.models.load_model('plantdesiesepredictionResnet50.h5')
+    return model
     
 def predict(model, img_path):
     # Load and preprocess the image
